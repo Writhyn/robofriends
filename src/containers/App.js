211a8f383eadb01f01bc126744f8b9045500d40e
Writhyn <writhyn@gmail.com>
2020-02-12 21:37:48 -0500

    prepping for git pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import 'tachyons';
 import './App.css';
 
@@ -33,7 +34,9 @@ class App extends React.Component {
         <h1 className='f1'>Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     )
