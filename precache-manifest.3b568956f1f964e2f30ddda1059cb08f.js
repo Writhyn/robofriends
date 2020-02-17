18e68213ec18d9d6887652f7ac2f5106fb855629
Writhyn <writhyn@gmail.com>
2020-02-17 18:23:20 -0500

    Updates

---
+++ precache-manifest.3b568956f1f964e2f30ddda1059cb08f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5caa6239bda2003ae1a24434bf3dbf",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b47f63618c2476b014d3",
+    "url": "/robofriends/static/css/main.ee66e90e.chunk.css"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/robofriends/static/js/2.2475d6f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.2475d6f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b47f63618c2476b014d3",
+    "url": "/robofriends/static/js/main.3c7ccfd3.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
