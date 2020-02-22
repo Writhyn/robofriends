7f0c4875bb2bef861881aac4f59c707aab881470
Writhyn <writhyn@gmail.com>
2020-02-22 11:45:01 -0500

    color

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({clicksy, name, email}) => {
   return (
-    <div className='bg-light-green dib br3 pa2 ma3 grow bw2 shadow-5' onClick={clicksy}>
+    <div className='bg-light-blue dib br3 pa2 ma3 grow bw2 shadow-5' onClick={clicksy}>
       <img src={`https://robohash.org/${name}?150x150`} alt=''></img>
       <div className='tc'>
         <h2>{name}</h2>
