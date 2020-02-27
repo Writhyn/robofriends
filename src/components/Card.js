d9fa3cb9d67c8b73d7f673d48db7b3ee886760fe
Writhyn <writhyn@gmail.com>
2020-02-26 19:23:16 -0500

    change pictures

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({clicksy, name, email}) => {
   return (
     <div id={name} className='bg-light-blue dib br3 pa2 ma3 grow bw2 shadow-5' onClick={clicksy}>
-      <img src={`https://robohash.org/${name}?150x150`} alt='robut' style={{pointerEvents: 'none'}}></img>
+      <img src={`https://robohash.org/${name}floop?150x150`} alt='robut' style={{pointerEvents: 'none'}}></img>
       <div className='tc' style={{pointerEvents: 'none'}}>
         <h2>{name}</h2>
         <p>{email}</p>
